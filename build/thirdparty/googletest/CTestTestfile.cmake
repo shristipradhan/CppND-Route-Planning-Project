95c9b2061b874c97879c05fb3b31bae8d5c49f11
shristi <shristi.pr@gmail.com>
2020-04-16 07:16:33 +0000

    Add gitignore

--- build/thirdparty/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shristi/Udacity/CppND-Route-Planning-Project/thirdparty/googletest
-# Build directory: /home/shristi/Udacity/CppND-Route-Planning-Project/build/thirdparty/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
